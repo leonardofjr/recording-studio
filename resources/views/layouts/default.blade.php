5fa5c7c63b0e6a4140ca5910b721b059e2a101fd
Leo <hello@leojr.me>
2020-01-01 18:14:07 -0500

    /

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>Cholo Life Studio - @yield('title')</title>
+        <title>Migtikueliz Beats - @yield('title')</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
